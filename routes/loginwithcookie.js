c8bae2307e8ac1990567f339f9af6b1f78827a0e
Eliud Gateri <egateri@gmail.com>
2023-07-17 13:01:08 +0300

    Added Cookie Auth

--- routes/loginwithcookie.js
+++ routes/loginwithcookie.js
@@ -2,12 +2,88 @@ const User = require("../model/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const express = require("express");
+const { serialize } =require('cookie');
 const router = express.Router();
 
 router.get("/loginwithcookie", (req, res) => {
   res
     .status(200)
     .json({ message: "To Login with Cookie. Please use the API POST Method" });
+});
+
+router.get("/loginwithcookietest", async (req, res) => {
+try {
+  
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(401).json({
+      status: 'error',
+      error: 'Request missing email or password',
+    });
+  }
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(400).json({ message: "User does not exists" });
+  }
+  console.log(process.env.NODE_ENV);
+  // res.json(user);
+
+  bcrypt.compare(password, user.password).then((isMatch) => {
+    if (isMatch) {
+      const payload = {
+        id: user._id,
+        email: user.email,
+        createdAt: user.createdAt,
+        username: user.username,
+        fullname: user.fullname,
+      };
+      jwt.sign(
+        payload,
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: 60 * 60 * 24 * 30,
+        },
+        (_err, token) => {
+          const serialized = serialize('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/',
+          });
+          res.setHeader('Set-Cookie', serialized);
+          res.status(200).json({
+            success: true,
+            user: {
+              email: payload.email,
+              username: payload.username,
+              fullname: payload.fullname,
+            },
+          });
+        },
+      );
+    } else {
+      res.status(400).json({
+        status: 'error',
+        error: 'Password and email does not match.',
+      });
+    }
+  });
+
+}
+catch (error) {
+
+  console.log(error);
+    res.status(500).json({
+      status: 'error',
+      error: 'Internal Server Error',
+    });
+ 
+}
+ 
 });
 
 router.post("/loginwithcookie", async (req, res) => {
